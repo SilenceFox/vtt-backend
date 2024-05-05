0236d64c5abdaffd9675ec47b924365cca68e0ff
SilenceFox <cristianplaybr@gmail.com>
2024-05-04 23:04:20 -0300

    Hack: Add chat module for future Chat API
    This commit also adds a new dependency, this is very much work in
    progress and many features are either missing or simply broken

---
+++ src/chat.rs
@@ -0,0 +1,92 @@
+use std::{sync::Arc, time};
+
+use chrono::{DateTime, Utc};
+use log::info;
+
+/// This module is focused on the CHAT API endpoint.
+/// TODO: GET chat messages
+/// TODO: GET polling
+/// TODO: POST new messages
+
+#[derive(Debug)]
+pub(crate) struct Chat {
+    users: Vec<User>,
+    message: Vec<Message>,
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct User {
+    username: String,
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct Message {
+    user: Arc<User>,
+    time: String,
+    message: String,
+}
+
+impl Chat {
+    pub fn new() -> Self {
+        Self::new_chat(Vec::new(), Vec::new())
+    }
+    fn new_chat(users: Vec<User>, message: Vec<Message>) -> Self {
+        Self { users, message }
+    }
+    fn user_join(new_user: User) {
+        todo!()
+    }
+    fn user_leave(removed_user: User) {
+        todo!()
+    }
+    fn add_to_history(&mut self, msg: Message) {
+        self.message.push(msg);
+    }
+    pub fn get_history(&self) {
+        for msg in &self.message {
+            println!("");
+            let formatted = format!(
+                "=========\nTime: {} \n {}: {}\n=========",
+                msg.time,
+                msg.user.username.to_uppercase(),
+                msg.message
+            );
+            println!("{formatted}")
+        }
+    }
+    pub(crate) fn send_message(&mut self, user: Arc<User>, msg: String) {
+        let user = user.clone();
+        let message = Message::new_message(user, msg);
+        self.add_to_history(message)
+    }
+}
+
+impl User {
+    /// Adds a new user
+    pub fn new_user(username: String) -> Arc<Self> {
+        let username = username.trim().to_string();
+        info!("New user {} has joined", &username);
+        Arc::new(Self { username })
+    }
+    /// Consumes the User struct and deletes it
+    pub fn delete_user(self) {
+        info!("The user {}, has been destroyed", self.username);
+        ()
+    }
+}
+
+impl Message {
+    pub fn new_message(user: Arc<User>, message: String) -> Self {
+        let time = Utc::now()
+            .with_timezone(&chrono_tz::America::Sao_Paulo)
+            .format("%d/%m [%H:%M:%S]")
+            .to_string();
+        let process_msg = message.trim().to_string();
+        let message = Self {
+            user,
+            time,
+            message: process_msg,
+        };
+        message
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::routable::Routable;
 use warp::Filter;
 mod character;
+pub mod chat;
 mod dice;
 mod routable;
 use log::{error, info, LevelFilter};
@@ -9,10 +10,31 @@ use log::{error, info, LevelFilter};
 async fn main() {
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
+    let mut chat = chat::Chat::new();
+    let user1 = chat::User::new_user("user1".to_string());
+    let user2 = chat::User::new_user("user2".to_string());
+
+    macro_rules! msg {
+        ($user:expr, $message:expr) => {{
+            let user_arc = $user.clone();
+            chat.send_message(user_arc, $message.to_string())
+        }};
+    }
+
+    chat.send_message(user1, "Salve".to_string());
+    chat.send_message(user2.clone(), "Eita bicho mensagem KKKKKKKKKK".to_string());
+    msg!(user2, "Whats up");
+
+    dbg!(&chat);
+    println!("");
+    println!("");
+    println!("");
+    chat.get_history();
+
     let dice_routes = dice::Menu::menu_routes();
-    let faced_roll = dice::Action::faced_roll();
+    let rolls = dice::Action::faced_roll().or(dice::Action::fate_roll());
     let character_routes = character::Menu::menu_routes();
-    let routes = character_routes.or(dice_routes).or(faced_roll);
+    let routes = character_routes.or(dice_routes).or(rolls);
 
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
