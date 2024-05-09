582d3e8c8a0fa15f8f5d585056ee17a438391d43
SilenceFox <cristianplaybr@gmail.com>
2024-05-09 02:52:46 -0300

    Remove clutter, Implement a working chat API
    Chat works, even if finicky and with bunch of clutter around, managed to
    make my way through the valley of Mutex, still, its work in progress and
    not pretty to look at.

--- src/chat.rs
+++ src/chat.rs
@@ -7,6 +7,7 @@ use std::sync::{Arc, Mutex};
 /// TODO: GET chat messages
 /// TODO: GET polling
 /// TODO: POST new messages
+/// DONE: POST Join chat
 
 #[derive(Debug)]
 pub(crate) struct Chat {
@@ -35,25 +36,31 @@ impl Chat {
     pub fn new() -> Self {
         Self::new_chat(Vec::new(), Vec::new())
     }
+
     fn new_chat(users: Vec<Arc<User>>, messages: Vec<Message>) -> Self {
         Self { users, messages }
     }
+
     fn check_user_exists(&self, username: &str) -> bool {
         self.users
             .iter()
             .any(|user| user.username == username.trim())
     }
+
     fn user_join(&mut self, new_user: &Arc<User>) {
         self.users.push(new_user.clone())
     }
+
     fn user_leave(&mut self, removed_user: &Arc<User>) {
         // Checks if the given user is in the chat and removes him
         info!("{} has left the chat.", removed_user.username);
         self.users.retain(|x| x.username != removed_user.username)
     }
+
     fn add_to_history(&mut self, msg: Message) {
         self.messages.push(msg);
     }
+
     fn get_history(&self) {
         // TODO: Truncate the history to 50 messages
         for msg in &self.messages {
@@ -68,10 +75,31 @@ impl Chat {
         }
         println!("╚════════════════════════════╛");
     }
-    fn send_message(&mut self, user: &Arc<User>, msg: String) {
+    fn get_last_message(&self) {
+        match self.messages.last() {
+            Some(msg) => {
+                let bar = "╰──────/MESSAGE-START/────────";
+
+                let formatted = format!(
+                    "│Time: {} \n│ |>{}: \n{}\n{}",
+                    msg.time,
+                    msg.user.username.to_uppercase(),
+                    bar,
+                    msg.message,
+                );
+                println!("╭─────────────────────────────");
+                println!("{formatted}");
+                println!("╰───────/MESSAGE-END/─────────")
+            }
+            None => (),
+        }
+    }
+
+    fn send_msg(&mut self, user: &Arc<User>, msg: &str) {
         let message = Message::new_message(user.clone(), msg);
         self.add_to_history(message)
     }
+
     fn get_users(&self) {
         if self.users.is_empty() {
             error!("No users are currently in the chat");
@@ -84,23 +112,22 @@ impl Chat {
         }
         // Iterate over each user in the chat and print a log
     }
+
     pub(crate) fn get_your_user(&self, username: &str) -> Option<&Arc<User>> {
         self.users
             .iter()
             .find(|user| user.username == username.trim())
     }
-    //
-    // API specific methods
-    //
 }
 
 impl User {
     /// Adds a new user
-    pub fn new_user(username: String) -> Arc<Self> {
+    pub fn new_user(username: &str) -> Arc<Self> {
         let username = username.trim().to_string();
         info!("New user {} has joined", &username);
         Arc::new(Self { username })
     }
+
     /// Consumes the User struct and deletes it
     pub fn delete_user(self) {
         info!("The user {}, has been destroyed", self.username);
@@ -109,22 +136,25 @@ impl User {
 }
 
 impl Message {
-    pub fn new_message(user: Arc<User>, message: String) -> Self {
+    pub fn new_message(user: Arc<User>, message: &str) -> Self {
         let time = Utc::now()
             .with_timezone(&chrono_tz::America::Sao_Paulo)
             .format("%d/%m [%H:%M:%S]")
             .to_string();
+
         let process_msg = message.trim().to_string();
+
         let message = Self {
             user,
             time,
             message: process_msg,
         };
+
         message
     }
 }
 
-pub(crate) fn handle_user_join(username: String, chat_state: &Arc<Mutex<Chat>>) {
+pub(crate) fn join_helper(username: &str, chat_state: &Arc<Mutex<Chat>>) {
     let mut chat = chat_state.lock().unwrap();
     if chat.check_user_exists(&username) {
         error!("User already exists in the chat");
@@ -134,30 +164,87 @@ pub(crate) fn handle_user_join(username: String, chat_state: &Arc<Mutex<Chat>>) 
     }
 }
 
-pub(crate) fn handle_user_leave(username: &Arc<User>, chat_state: &Arc<Mutex<Chat>>) {
+pub(crate) fn leave_helper(username: &Arc<User>, chat_state: &Arc<Mutex<Chat>>) {
     let mut chat = chat_state.lock().unwrap();
+
     match chat.check_user_exists(&username.username) {
-        true => {
-            info!("User: {} has left the chat.", &username.username);
-            chat.user_leave(username)
-        }
+        true => chat.user_leave(username),
         false => error!("User: {} does not exist in the chat.", &username.username),
     }
 }
-pub(crate) fn handle_user_send_message(
+pub(crate) fn send_message_helper(
     username: &Arc<User>,
-    message: String,
+    message: &str,
     chat_state: &Arc<Mutex<Chat>>,
 ) {
     let mut chat = chat_state.lock().unwrap();
-    if chat.check_user_exists(&username.username) {
-        info!(
-            "Message sent from: {}\nMessage: {}",
-            &username.username, &message
-        );
-        chat.send_message(username, message)
-    } else {
+
+    if !chat.check_user_exists(&username.username) {
+        info!("User does not exist in the chat. Creating new user...");
         chat.user_join(username);
-        chat.send_message(username, message)
+        info!("User created: {}", &username.username);
+    };
+
+    info!(
+        "Message sent from: {}\nMessage: {}",
+        &username.username, &message
+    );
+
+    chat.send_msg(username, message)
+}
+
+/// Export all API handlers and routes for the chat
+pub mod api {
+    use std::borrow::BorrowMut;
+
+    use super::*;
+    use axum::{extract::State, Json};
+    use serde_json::{json, Value};
+
+    pub(crate) enum Errors {
+        InvalidUsername,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub(crate) struct SendMessageRequest {
+        message: String,
+        username: String,
+    }
+
+    // Join the chat with axum
+    pub(crate) async fn join(
+        State(chat): State<Arc<Mutex<Chat>>>,
+        Json(req): Json<String>,
+    ) -> Json<String> {
+        // Check username for invalid characters
+        let user = req.trim();
+        join_helper(user, &chat);
+
+        let json = Json("Request received".to_string());
+        json
+    }
+
+    pub(crate) async fn send_message(
+        chat: State<Arc<Mutex<Chat>>>,
+        Json(req): Json<SendMessageRequest>,
+    ) -> Json<String> {
+        let msg = req.message;
+        let usr = req.username;
+        // println!("{}: {}", usr, msg);
+
+        let mut guard_chat = chat.lock().unwrap();
+
+        if !guard_chat.check_user_exists(&usr) {
+            // If the user exists, get the user and send the message
+            guard_chat.user_join(&User::new_user(&usr));
+        }
+        let my_usr = guard_chat.get_your_user(&usr).unwrap().clone();
+        guard_chat.send_msg(&my_usr, &msg);
+        guard_chat.get_last_message();
+        Json("Message Sent".to_string())
+    }
+
+    async fn leave(Json(req): Json<User>, chat: State<&Arc<Mutex<Chat>>>) -> Result<(), Errors> {
+        todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![allow(dead_code, unused_variables)]
 use std::sync::{Arc, Mutex};
 
-use axum::{routing::get, Router};
-use chat::{handle_user_join, handle_user_leave, handle_user_send_message};
+use axum::{
+    handler::Handler,
+    http::HeaderMap,
+    routing::{get, post},
+    Router,
+};
+use chat::{join_helper, leave_helper, send_message_helper, User};
 mod character;
-pub mod chat;
+mod chat;
 mod dice;
 use log::{error, info, LevelFilter};
 
@@ -13,30 +18,16 @@ async fn main() {
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
     let chat_state = Arc::new(Mutex::new(chat::Chat::new()));
-    handle_user_join(String::from("Joao"), &chat_state);
-    let get_my_user = |state: &Arc<Mutex<chat::Chat>>| -> Arc<chat::User> {
-        let chat = state.lock().unwrap();
-        let my_user = chat.get_your_user("Joao").unwrap();
-        my_user.clone()
-    };
-    handle_user_send_message(&get_my_user(&chat_state), "Yoooo".to_string(), &chat_state);
-    handle_user_send_message(
-        &get_my_user(&chat_state),
-        "What is love".to_string(),
-        &chat_state,
-    );
-    handle_user_leave(&get_my_user(&chat_state), &chat_state);
 
-    let app = Router::new().route("/dice", get(dice::menu_routes));
+    let app = Router::new()
+        .route("/dice", get(dice::menu_routes))
+        .route("/dice/faced", post(dice::Action::faced_roll))
+        .route("/dice/fate", post(dice::Action::fate_roll))
+        .route("/chat/join", post(chat::api::join))
+        .route("/chat/msg", post(chat::api::send_message))
+        .with_state(chat_state);
 
+    println!("Server started on http://localhost:3030");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
-    // let dice_routes = dice::Menu::menu_routes();
-    // let rolls = dice::Action::faced_roll().or(dice::Action::fate_roll());
-    // let character_routes = character::Menu::menu_routes();
-    // let routes = character_routes.or(dice_routes).or(rolls);
-    //
-    // warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
-    //
 }
